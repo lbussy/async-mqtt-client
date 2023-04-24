266a5273e033db6e06311f2f45022a4e390a51ba
LBussy <lee@bussy.org>
2023-04-24 13:19:56 -0500

    Update test script

--- examples/FullyFeatured-ESP32/FullyFeatured-ESP32.ino
+++ examples/FullyFeatured-ESP32/FullyFeatured-ESP32.ino
@@ -1,135 +1,158 @@
-/*
-This example uses FreeRTOS softwaretimers as there is no built-in Ticker library
-*/
-
-
+#include <LCBUrl.h>
+#include <Arduino.h>
+#include <list>
+#include <ArduinoLog.h>
 #include <WiFi.h>
-extern "C" {
-	#include "freertos/FreeRTOS.h"
-	#include "freertos/timers.h"
-}
-#include <AsyncMqttClient.h>
+#include <Ticker.h>
 
-#define WIFI_SSID "yourSSID"
-#define WIFI_PASSWORD "yourpass"
+#ifdef LCBURL_MDNS
+#include <ESPmDNS.h>
+#endif
 
-#define MQTT_HOST IPAddress(192, 168, 1, 10)
-#define MQTT_PORT 1883
+#include "AsyncMqttClient.h"
+
+#if __has_include("./secrets.h")
+#include "secrets.h" // Include for AP_NAME and PASSWD below
+const char *ssid = AP_NAME;
+const char *password = PASSWRD;
+const char *hostname = HOSTNAME;
+const char *mqtt_host = MQTT_HOST;
+const int mqtt_port = MQTT_PORT;
+const int baud = BAUD;
+#else
+const char *ssid = "my_ap";
+const char *password = "passsword";
+const char *hostname = "test-host";
+const char *mqtt_host = "raspberrypi.local";
+const int mqtt_port = 1883;
+const int baud = 115200;
+#endif
+
+LCBUrl url;
+bool oktomqt;
 
 AsyncMqttClient mqttClient;
-TimerHandle_t mqttReconnectTimer;
-TimerHandle_t wifiReconnectTimer;
+Ticker mqttReconnectTimer;
 
-void connectToWifi() {
-  Serial.println("Connecting to Wi-Fi...");
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+void connectToWifi()
+{
+    Log.notice("Connecting to Wi-Fi." CR);
+    WiFi.setAutoReconnect(false);
+    WiFi.setSleep(false);
+    WiFi.mode(WIFI_MODE_STA);
+    WiFi.begin(ssid, password);
 }
 
-void connectToMqtt() {
-  Serial.println("Connecting to MQTT...");
-  mqttClient.connect();
+void connectToMqtt()
+{
+    Log.notice("Connecting to MQTT." CR);
+    mqttClient.connect();
 }
 
-void WiFiEvent(WiFiEvent_t event) {
-    Serial.printf("[WiFi-event] event: %d\n", event);
-    switch(event) {
+void WiFiEvent(WiFiEvent_t event)
+{
+    Log.notice("[WiFi-event] event: %d" CR, event);
+    switch (event)
+    {
     case SYSTEM_EVENT_STA_GOT_IP:
-        Serial.println("WiFi connected");
-        Serial.println("IP address: ");
-        Serial.println(WiFi.localIP());
-        connectToMqtt();
+        Log.notice("WiFi connected. IP address: %s" CR, WiFi.localIP().toString().c_str());
+        if (oktomqt)
+            connectToMqtt();
         break;
     case SYSTEM_EVENT_STA_DISCONNECTED:
-        Serial.println("WiFi lost connection");
-        xTimerStop(mqttReconnectTimer, 0); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
-        xTimerStart(wifiReconnectTimer, 0);
+        Log.notice("WiFi lost connection." CR);
+        // Ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
+        mqttReconnectTimer.detach();
+        connectToWifi();
         break;
     }
 }
 
-void onMqttConnect(bool sessionPresent) {
-  Serial.println("Connected to MQTT.");
-  Serial.print("Session present: ");
-  Serial.println(sessionPresent);
-  uint16_t packetIdSub = mqttClient.subscribe("test/lol", 2);
-  Serial.print("Subscribing at QoS 2, packetId: ");
-  Serial.println(packetIdSub);
-  mqttClient.publish("test/lol", 0, true, "test 1");
-  Serial.println("Publishing at QoS 0");
-  uint16_t packetIdPub1 = mqttClient.publish("test/lol", 1, true, "test 2");
-  Serial.print("Publishing at QoS 1, packetId: ");
-  Serial.println(packetIdPub1);
-  uint16_t packetIdPub2 = mqttClient.publish("test/lol", 2, true, "test 3");
-  Serial.print("Publishing at QoS 2, packetId: ");
-  Serial.println(packetIdPub2);
+void onMqttConnect(bool sessionPresent)
+{
+    Log.notice("Connected to MQTT." CR);
+    Log.notice("Session present: %T" CR, sessionPresent);
+    uint16_t packetIdSub = mqttClient.subscribe("test/lol", 2);
+    Log.notice("Subscribing at QoS 2, packetId: %d" CR, packetIdSub);
+    mqttClient.publish("test/lol", 0, true, "test 1");
+    Log.notice("Publishing at QoS 0" CR);
+    uint16_t packetIdPub1 = mqttClient.publish("test/lol", 1, true, "test 2");
+    Log.notice("Publishing at QoS 1, packetId: %d" CR, packetIdPub1);
+    uint16_t packetIdPub2 = mqttClient.publish("test/lol", 2, true, "test 3");
+    Log.notice("Publishing at QoS 2, packetId: %d" CR, packetIdPub2);
+    mqttReconnectTimer.detach();
+    mqttReconnectTimer.attach(2, connectToMqtt);
 }
 
-void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
-  Serial.println("Disconnected from MQTT.");
-
-  if (WiFi.isConnected()) {
-    xTimerStart(mqttReconnectTimer, 0);
-  }
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason)
+{
+    Log.notice("Disconnected from MQTT." CR);
 }
 
-void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
-  Serial.println("Subscribe acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
-  Serial.print("  qos: ");
-  Serial.println(qos);
+void onMqttSubscribe(uint16_t packetId, uint8_t qos)
+{
+    Log.notice("Subscribe acknowledged." CR);
+    Log.notice("\tpacketId: %d" CR, packetId);
+    Log.notice("\tqos: %d" CR, qos);
 }
 
-void onMqttUnsubscribe(uint16_t packetId) {
-  Serial.println("Unsubscribe acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
+void onMqttUnsubscribe(uint16_t packetId)
+{
+    Log.notice("Unsubscribe acknowledged." CR);
+    Log.notice("\tpacketId: %d" CR, packetId);
 }
 
-void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
-  Serial.println("Publish received.");
-  Serial.print("  topic: ");
-  Serial.println(topic);
-  Serial.print("  qos: ");
-  Serial.println(properties.qos);
-  Serial.print("  dup: ");
-  Serial.println(properties.dup);
-  Serial.print("  retain: ");
-  Serial.println(properties.retain);
-  Serial.print("  len: ");
-  Serial.println(len);
-  Serial.print("  index: ");
-  Serial.println(index);
-  Serial.print("  total: ");
-  Serial.println(total);
+void onMqttMessage(char *topic, char *payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total)
+{
+    Log.notice("Publish received." CR);
+    Log.notice("\ttopic: %s", topic);
+    Log.notice("\tqos: %d" CR, properties.qos);
+    Log.notice("\tdup: %T" CR, properties.dup);
+    Log.notice("\tretain: %T" CR, properties.retain);
+    Log.notice("\tlen: %d" CR, len);
+    Log.notice("\tindex: " CR, index);
+    Log.notice("\ttotal: %d" CR, total);
 }
 
-void onMqttPublish(uint16_t packetId) {
-  Serial.println("Publish acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
+void onMqttPublish(uint16_t packetId)
+{
+    Log.notice("Publish acknowledged.");
+    Log.notice("\tpacketId: %d" CR, packetId);
 }
 
-void setup() {
-  Serial.begin(115200);
-  Serial.println();
-  Serial.println();
+void setup()
+{
+    oktomqt = false;
+    Serial.begin(BAUD);
+    Serial.flush();
+    Serial.println();
+    delay(1000);
 
-  mqttReconnectTimer = xTimerCreate("mqttTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt));
-  wifiReconnectTimer = xTimerCreate("wifiTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToWifi));
+    // Initialize with log level and log output.
+    Log.begin(LOG_LEVEL_VERBOSE, &Serial);
+    Log.verbose(F("Starting." CR));
 
-  WiFi.onEvent(WiFiEvent);
+    WiFi.onEvent(WiFiEvent);
+    connectToWifi();
+    while (!WiFi.isConnected()) {;}
 
-  mqttClient.onConnect(onMqttConnect);
-  mqttClient.onDisconnect(onMqttDisconnect);
-  mqttClient.onSubscribe(onMqttSubscribe);
-  mqttClient.onUnsubscribe(onMqttUnsubscribe);
-  mqttClient.onMessage(onMqttMessage);
-  mqttClient.onPublish(onMqttPublish);
-  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+    MDNS.begin(hostname);
 
-  connectToWifi();
+    url.setUrl(mqtt_host);
+    while (url.getIP(mqtt_host) == INADDR_NONE) {;}
+
+    Log.verbose("Setting MQTT Host as %s, (%s)" CR, mqtt_host, url.getIP(mqtt_host).toString().c_str());
+    mqttClient.onConnect(onMqttConnect);
+    mqttClient.onDisconnect(onMqttDisconnect);
+    mqttClient.onSubscribe(onMqttSubscribe);
+    mqttClient.onUnsubscribe(onMqttUnsubscribe);
+    mqttClient.onMessage(onMqttMessage);
+    mqttClient.onPublish(onMqttPublish);
+    mqttClient.setServer(url.getIP(mqtt_host), mqtt_port);
+    connectToMqtt();
+    oktomqt = true;
 }
 
-void loop() {
+void loop()
+{
 }
